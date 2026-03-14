71db8443f7df474788674a661e513854093667e5
gazitasnimahmad <39596910+gazitasnimahmad@users.noreply.github.com>
2026-03-15 01:29:15 +0530

    updated ip

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -16,7 +16,7 @@ function Homepage() {
   function handleSubmit(event) {
     setIsLoading(true);
     event.preventDefault();
-    const url = "http://172.20.10.14:8080/stockhelper/upload";
+    const url = "http://192.168.1.9:8080/stockhelper/upload";
     // const url = "https://stock-helper-dev.onrender.com/stockhelper/upload";
     const formData = new FormData();
     formData.append("file", file);
